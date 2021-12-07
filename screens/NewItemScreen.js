467d7e5f2facc26c1f1ef9d676817549ca0b851d
Jacob Wilson <thisisjacobwilson@gmail.com>
2021-12-07 13:03:41 -0500

    Set up some screens and components

---
+++ screens/NewItemScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewItemScreen() {
+  return (
+    <div>
+      This is page where the NewEdit component goes.
+    </div>
+  )
+}
---
+++ screens/QuestDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function QuestDetails() {
+  return (
+    <div>
+      gesture handler goes here  also with buttons for delete and possible edit.
+    </div>
+  )
+}
