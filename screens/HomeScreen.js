84a6c0b837fa471d278157adbdfee31f789bb7bc
Jacob Wilson <thisisjacobwilson@gmail.com>
2021-12-11 20:04:22 -0500

    Data is being split to homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
 import { useData } from '../components/context/Context'
 import Task from '../components/Task';
 
 export default function HomeScreen() {
   const [data, updateData] = useData()
-  
+  const [nonComplete, setNonComplete] = useState()
   //stack navigation here with name list and name details
+  
+useEffect(()=>{
+  setNonComplete(data.filter(el=> el.taskCompleted===false ))
+},[data])
 
   return (
     <SafeAreaView style={styles.container} edges={['right', 'bottom', 'left']}>
@@ -14,11 +19,11 @@ export default function HomeScreen() {
         We'll put the flatlist here that has a pressable that shows the
         QuestDetails screen on press
       </Text>
-      <FlatList data={data}
-      renderItem={item => <Task task={item}
-      ListEmptyComponent={<Text>No Data. Such Sad.</Text>}    
+      <FlatList data={nonComplete}
+      renderItem={item => <Task task={item}/>}
       keyExtractor={(item, index)=> item.taskTitle + "-" + index}  
-      />}/>
+      ListEmptyComponent={<Text>No Data. Such Sad.</Text>}    
+      />
     </SafeAreaView>
   );
 }
