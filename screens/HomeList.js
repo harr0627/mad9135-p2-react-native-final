5cb0425ce6e277e719507641b8260ef0d8e15e46
Jacob Wilson <thisisjacobwilson@gmail.com>
2021-12-17 12:35:25 -0500

    completed store review

--- screens/HomeList.js
+++ screens/HomeList.js
@@ -5,6 +5,7 @@ import {
   SafeAreaView,
   StyleSheet,
   ActivityIndicator,
+  Platform,
 } from 'react-native';
 import Task from '../components/Task';
 import { useData } from '../components/context/Context';
@@ -12,20 +13,94 @@ import { useState, useEffect } from 'react';
 import styles from '../components/Styles/Styles';
 import { useFonts, Raleway_500Medium } from '@expo-google-fonts/raleway';
 import { SourceSansPro_400Regular } from '@expo-google-fonts/source-sans-pro';
+import * as StoreReview from 'expo-store-review';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+
 
 export default function HomeList({ navigation }) {
   const [data, updateData] = useData([]);
   const [nonComplete, setNonComplete] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [reviewCounter, setReviewCounter] = useState(0)
   let [fontsLoaded] = useFonts({
     Raleway_500Medium,
     SourceSansPro_400Regular,
   });
+  let storeUrl = StoreReview.storeUrl()
+  const { getItem, setItem } = useAsyncStorage('ExecMethodsQuestCounter');
+
+  const callReview = () => {
+    StoreReview.isAvailableAsync()
+    .then((res1)=>{
+      return (StoreReview.hasAction(), res1)
+    })
+    .then((res2, res1) => {
+      if(res1 === true || res2 === true){
+        //only one needs to be true, requestReview will handle navigation either way.
+        StoreReview.requestReview()
+      } else {
+        throw "No Review Available"
+      }
+    })
+    .catch((err)=>{
+      console.error(err);
+    })
+    
+    // if(reviewCounter === 32){
+    //   StoreReview.isAvailableAsync()
+    //   .then((yesAsync)=>{
+    //     if(yesAsync){
+    //       return StoreReview.hasAction().then((yesAction)=>{
+    //         if(yesAction){
+    //           StoreReview.requestReview()
+    //         }
+    //       })
+    //       .catch(()=>{
+    //         console.log("is Not Available Async")
+    //       })
+    //     }else{
+    // return
+    // }})
+    //     .catch((er)=>{
+    //       console.log(er);
+    //     })
+
+  }
+  
+
+  const checkStorageCounter = async () => {
+    getItem()
+    .then((number)=>{
+      number = number === null? 0 : JSON.parse(number)
+      setReviewCounter(number)
+    })
+    .catch((er)=>{
+      console.error(er);
+    })
+  }
+
+  const setStorageCounter = async (newNum) => {
+    setItem(JSON.stringify(newNum))
+    console.log(reviewCounter+1)
+  }
 
   useEffect(() => {
     setNonComplete(data.filter((el) => el.taskCompleted === false));
     setIsLoading(false);
   }, [data]);
+
+  useEffect(() => {
+    checkStorageCounter();
+    if(reviewCounter === 10)
+    {callReview()}
+  }, [])
+
+  useEffect(() => {
+    console.log("Counter",reviewCounter)
+    setStorageCounter(reviewCounter+1);
+  }, [reviewCounter])
+
+
 
   if (!fontsLoaded) {
     return null;
